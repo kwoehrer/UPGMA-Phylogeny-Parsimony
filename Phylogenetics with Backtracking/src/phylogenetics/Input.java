5e5fbbad75fb6163240c4aea57e2aa3a03c6eaa0
Krischan Woehrer <60715629+kwoehrer@users.noreply.github.com>
2021-08-22 15:38:55 -0700

    Add files via upload
    -Added backtracking to parsimony methodology 
    -Added a generalized likelyhood-based hypothesis to guess what a minimum parsimony tree could be and start parsimony calculations with this hypothesis
             -This accelerates and removes a multitude of unnecessary recursive method calls.

---
+++ Phylogenetics with Backtracking/src/phylogenetics/Input.java
@@ -0,0 +1,189 @@
+package phylogenetics;
+
+///////////////////////// TOP OF FILE COMMENT BLOCK ////////////////////////////
+//
+//Title: UPGMA based Phylogenetic Tree Creator
+//
+//Author: Krischan Woehrer
+//Email: kwoehrer@wisc.edu
+//
+///////////////////////////////// NOTES ////////////////////////////////////////
+//
+//Reasons for creating this program:
+//1. I remember taking microbiology-470 at UW-Madison and one of our big
+//projects for the semester was to create the most parsiminious
+//phylogenetic tree of 20-25 randomly named species when we were only given
+//the presence of various traits (I remember it being close to 10-12).
+//Everyone in the course spent hours upon hours manually crafting their
+//trees and I remember thinking to myself how nice it would be if a
+//computer could help compute the optimal state for parsimony.
+//Unfortunately, almost all the programs found online either costed money
+//or their algorithms were faulty and yielded trees that were close to
+//the most parsiminious tree but not quite there. Now that I am pursuing
+//a career in computer science and developement, I have decided to solve
+//this problem on my own and offer it as a free resource online.
+//2. Gain a greater understanding of binary trees and their algorithms
+//3. Practice my skills learned from my coursework in computer science.
+//Things to improve/TODO:
+//1. Numerous points in the program could be better optimized to run a
+//little bit faster. These are noted locally when they occur.
+//2. When determining the most parsiminious trait states, I currently use
+//a brute-force approach. Although I narrow down the number of computations
+//by determining traits of ancestors we know for certain, I believe there
+//are additional ways I can cut down on the number of computations and speed
+//the program up.
+//A brute-force approach will likely still be required in order construct
+//the most parsiminious phylogenetic tree accurately.
+//
+//
+///////////////////////////////// CITATIONS ////////////////////////////////////
+//
+//Source or Recipient; Description
+//UW-Madison Microbiology-470
+//This class taught me how to do UPGMA based phylogeny as part of my course work.
+//
+/////////////////////////////// 80 COLUMNS WIDE /////////////////////////////////**
+
+import java.util.Scanner;
+import java.io.FileNotFoundException;
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * This class contains the methods for taking the inputFile.txt and converting it
+ * to usable data for the program.
+ * @author Krischan Woehrer
+ *
+ */
+
+public class Input {
+
+    /**
+     * This method creates an array of species object from a user.txt file. 
+     * @param args
+     * @return
+     */
+    public static Species[] transposeFile(String[] args) {
+        System.out.println("Transposing Input File...");
+        Species[] descendants = null;
+        Scanner scnr = null;
+        File file = new File(args[0]);
+        int numberOfSpecies = 0;
+        int numberOfTraits = 0;
+        Scanner bufferScnr = null;
+
+        try {
+            scnr = new Scanner(file);
+
+            if (scnr.hasNextLine()) {
+                String firstLineOfInput = scnr.nextLine();
+
+                bufferScnr = new Scanner(firstLineOfInput);
+
+                if (bufferScnr.hasNextInt()) {
+                    numberOfSpecies = bufferScnr.nextInt();
+                    System.out.println("\tAcquired number of species.");
+                } else {
+                    throw new Exception(
+                        "The first line should contain the following: \" numberOfSpecies numberOfTraitsInSpecies\""
+                            + "\n The program detected that either a non-integer was entered or no tokens were present");
+                }
+
+                if (bufferScnr.hasNextInt()) {
+                    numberOfTraits = bufferScnr.nextInt();
+                    System.out.println("\tAcquired number of comparable traits.");
+                } else {
+                    throw new Exception(
+                        "The first line should contain the following: \" numberOfSpecies numberOfTraitsInSpecies\""
+                            + "\n The program detected that either a non-integer was entered or no integer was present for the number"
+                            + "of traits.");
+                }
+
+                if (bufferScnr.hasNext()) {
+                    throw new Exception(
+                        "First line should only contain the following: \"numberOfSpecies numberOfTraits\"");
+                }
+                bufferScnr.close();
+                // Initializes the descendant array
+                descendants = new Species[numberOfSpecies];
+            } else {
+                System.out.println("File is empty");
+            }
+
+
+            int indexOfDescendantArr = 0;
+            System.out.println("\tTransposing species names and traits...");
+            while (scnr.hasNextLine()) {
+                int[] traitArr = new int[numberOfTraits];
+                String name;
+
+                if (indexOfDescendantArr >= descendants.length) {
+                    throw new Exception(
+                        "There are more species, or more lines entered, on the text document than specified"
+                            + " by the user.");
+                }
+
+                if (scnr.hasNextInt()) {
+                    throw new Exception(
+                        "The first token entered on each line should be the species name. Cannot be an integer.");
+                }
+                name = scnr.next();
+
+                try {
+                    // Used this in case extra traits are entered or not enough traits are entered
+                    // for each species. Could have used String.split and Integer.parseInt() methods
+                    // but that wouldn't allow for as precise error generation.
+                    for (int i = 0; i < numberOfTraits; i++) {
+                        if (scnr.hasNextInt()) {
+                            traitArr[i] = scnr.nextInt();
+                            // Check to make sure its a 1 or 2
+                            if (traitArr[i] != 1 && traitArr[i] != 0) {
+                                throw new Exception(
+                                    "Only 0's (trait not present) or 1's (trait is present) should"
+                                        + "be entered in for trait values.\n This error was generated by species \""
+                                        + name + "\".");
+                            }
+                        } else {
+                            throw new Exception(
+                                "Only 0's (trait not present) or 1's (trait is present) should"
+                                    + "be entered in for trait values.\n Either a non-integer was found entered in species \""
+                                    + name + "\", or there were not enough traits in species \""
+                                    + name + "\". This species had " + i + " traits entered when this error occured (should have"
+                                        + numberOfTraits + " traits present.");
+                        }
+                    }
+                } catch (Exception e) {
+                    System.out.println(e.getMessage());
+                    return null;
+                }
+                // Create new species from the information we parsed and add it to array of
+                // descendants
+                Species currSpecies = new Species(name, traitArr, true);
+                descendants[indexOfDescendantArr] = currSpecies;
+                indexOfDescendantArr++;
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found, please check to see if file is in correct location "
+                + "and it is spelled correctly.");
+            e.printStackTrace();
+            return null;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+            return null;
+        } finally {
+            if (scnr != null) {
+                scnr.close();
+            }
+            if (bufferScnr != null) {
+                bufferScnr.close();
+            }
+        }
+        System.out.println("\tDone Transposing Input File!");
+        return descendants;
+    }
+
+}
---
+++ Phylogenetics with Backtracking/src/phylogenetics/Parsimony.java
@@ -0,0 +1,360 @@
+
+package phylogenetics;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+///////////////////////// TOP OF FILE COMMENT BLOCK ////////////////////////////
+//
+// Title: UPGMA based Phylogenetic Tree Creator
+//
+// Author: Krischan Woehrer
+// Email: kwoehrer@wisc.edu
+//
+///////////////////////////////// NOTES ////////////////////////////////////////
+//
+// Reasons for creating this program:
+// 1. I remember taking microbiology-470 at UW-Madison and one of our big
+// projects for the semester was to create the most parsiminious
+// phylogenetic tree of 20-25 randomly named species when we were only given
+// the presence of various traits (I remember it being close to 10-12).
+// Everyone in the course spent hours upon hours manually crafting their
+// trees and I remember thinking to myself how nice it would be if a
+// computer could help compute the optimal state for parsimony.
+// Unfortunately, almost all the programs found online either costed money
+// or their algorithms were faulty and yielded trees that were close to
+// the most parsiminious tree but not quite there. Now that I am pursuing
+// a career in computer science and developement, I have decided to solve
+// this problem on my own and offer it as a free resource online.
+// 2. Gain a greater understanding of binary trees and their algorithms
+// 3. Practice my skills learned from my coursework in computer science.
+// Things to improve/TODO:
+// 1. Numerous points in the program could be better optimized to run a
+// little bit faster. These are noted locally when they occur.
+// 2. When determining the most parsiminious trait states, I currently use
+// a brute-force approach. Although I narrow down the number of computations
+// by determining traits of ancestors we know for certain, I believe there
+// are additional ways I can cut down on the number of computations and speed
+// the program up.
+// A brute-force approach will still be required in order construct
+// the most parsiminious phylogenetic tree accurately.
+//
+//
+///////////////////////////////// CITATIONS ////////////////////////////////////
+//
+// Source or Recipient; Description
+// UW-Madison Microbiology-470
+// This class taught me how to do UPGMA based phylogeny as part of my course work.
+//
+/////////////////////////////// 80 COLUMNS WIDE /////////////////////////////////**
+
+public class Parsimony {
+
+    private static int[] indexOfMinimumParsimonyTree;
+    private static int[] indexOfAncestorTracker;
+    private static int minParsimonyScore;
+
+    /**
+     * This method takes a rooted tree with ancestor nodes that have an undetermined trait state and
+     * determines the trait states that will result in minimum parsimony of the tree.
+     * 
+     * @param tree
+     * @return
+     */
+    public static int findMostParsimoniousTree(RootedTree tree) {
+        int numOfAncestors = tree.size() - tree.getInitialSpecies().size();
+        Species[] ancestorNodes = new Species[numOfAncestors];
+        indexOfMinimumParsimonyTree = new int[numOfAncestors];
+        indexOfAncestorTracker = new int[numOfAncestors];
+        minParsimonyScore = Integer.MAX_VALUE;
+
+        // Inserts all ancestor nodes into the species array ancestorNodes
+        findAncestorNodes(tree.getRoot(), ancestorNodes, 0);
+        // Create a hypothesis of what best state could be based off of prevalence of traits of
+        // initial descendants
+        // Note: This will not find the most parsiminious tree but drastically reduces the time to
+        // calculate the most parsimonious tree by accelerating the rate at which the backtracking
+        // algorithm
+        // performs
+        maximumLikelyHoodTraitStates(ancestorNodes);
+
+        // Finds optimal trait states of ancestor nodes
+        findOptimalTraitStates(numOfAncestors, ancestorNodes);
+        // Changes the actual trait array in the species object to its optimal configuration
+        changeTreeTraitsToMinPars(ancestorNodes);
+
+        return minParsimonyScore;
+    }
+
+    /**
+     * Adds all ancestor species into an array. Iterates from top left to bottom right.
+     * Index = 0 is the root of the tree.
+     * @param Species Current species to add into the array. This method also takes the descendants
+     *      of this species and adds them to the speciesArr array if they are also ancestor nodes.
+     * @param speciesArr Array of ancestor species nodes to be populated by this method.
+     * @param currIndex Current index of the array.
+     */
+    private static int findAncestorNodes(Species species, Species[] speciesArr, int currIndex) {
+        // Puts current species into array
+        speciesArr[currIndex] = species;
+        currIndex++;
+        // Gets Descendant species
+        Species leftDescendant = species.getDescendants()[0];
+        Species rightDescendant = species.getDescendants()[1];
+
+        // If descendants aren't ancestor nodes then stop, if they are ancestor nodes than add them
+        // to array
+        if (leftDescendant.getHeight() != 1) {
+            currIndex = findAncestorNodes(leftDescendant, speciesArr, currIndex);
+        }
+
+        if (rightDescendant.getHeight() != 1) {
+            currIndex = findAncestorNodes(rightDescendant, speciesArr, currIndex);
+        }
+
+        return currIndex;
+    }
+
+    /**
+     * This method determines the most likely trait states of an array of ancestor nodes based
+     * off of the initial descendants (at tree instantation) trait values.
+     * @param ancestorNodes An array containing all ancestor nodes of the tree.
+     */
+    private static void maximumLikelyHoodTraitStates(Species[] ancestorNodes) {
+        // Can only determine the traits in this way for an ancestor node with a height of greater
+        // than 3 as these
+        for (int i = 0; i < ancestorNodes.length; i++) {
+            if (ancestorNodes[i].getHeight() > 2) {
+                // Finds likely trait state
+                int[] likelyTraitState = findLikelyTraitState(ancestorNodes[i]);
+                ancestorNodes[i].setTraits(likelyTraitState);
+                // Matches trait state to a possible trait state and returns index of that possible
+                // trait state
+                // Adds this index to its appropriate index in indexOfMinimumParsimonyTree
+                indexOfMinimumParsimonyTree[i] =
+                    getTraitStateIndex(likelyTraitState, ancestorNodes[i]);
+            }
+        }
+
+        // Determine most likely traits for ancestor nodes with a height of 2. This method uses the
+        // ancestor of
+        // this ancestor node as we cannot infer anything from an ancestor node with only two
+        // descendants.
+        // We cannot do so as the ancestor node trait states are both equally likely and either
+        // option would increase
+        // parsimony score of the tree by 1. By keeping the trait value the same as the trait value
+        // in ancestor of this
+        // ancestor node, we avoid adding another parsimony score.
+        for (int i = 0; i < ancestorNodes.length; i++) {
+            if (ancestorNodes[i].getHeight() == 2) {
+                int[] currAncestorNodeTraits = ancestorNodes[i].getTraits();
+                for (int j = 0; j < currAncestorNodeTraits.length; j++) {
+                    // Replaces any -1 traits with the ancestors (of this node) trait value at that
+                    // index
+                    if (currAncestorNodeTraits[j] == -1) {
+                        currAncestorNodeTraits[j] = ancestorNodes[i].getAncestor().getTraits()[j];
+                    }
+                }
+                // Matches trait state to a possible trait state and returns index of that possible
+                // trait state
+                // Adds this index to its appropriate index in indexOfMinimumParsimonyTree
+                indexOfMinimumParsimonyTree[i] =
+                    getTraitStateIndex(currAncestorNodeTraits, ancestorNodes[i]);
+            }
+        }
+        // Store minimum parsimony score
+        minParsimonyScore = getParsimonyScore(ancestorNodes, 0);
+    }
+
+    /**
+     * This method finds the most likely trait state of an ancestor node based off of its descendants that
+     * were initial species entered at tree instantiation.
+     * @param ancestor An ancestor node species.
+     * @return An int array that contains the most likely trait state.
+     */
+    private static int[] findLikelyTraitState(Species ancestor) {
+        ArrayList<Species> initialSpeciesThatAreDescendants = new ArrayList<Species>();
+        getInitialSpeciesThatAreDescendants(ancestor, initialSpeciesThatAreDescendants);
+
+        double[] maximumLikelyhoodTraitStateDbl = new double[ancestor.getTraits().length];
+        int[] maximumLikelyhoodTraitStateInt = new int[ancestor.getTraits().length];
+
+        // Adds the value of all traits of all initial species that are descendants of the ancestor
+        for (int i = 0; i < initialSpeciesThatAreDescendants.size(); i++) {
+            Species currentDescendant = initialSpeciesThatAreDescendants.get(i);
+            int[] currentDescendantTraits = currentDescendant.getTraits();
+
+            for (int j = 0; j < currentDescendantTraits.length; j++) {
+                maximumLikelyhoodTraitStateDbl[j] += currentDescendantTraits[j];
+            }
+        }
+
+        // Calculates if the traits are more likely to be present(1) or absent (0) in ancestor
+        for (int i = 0; i < maximumLikelyhoodTraitStateDbl.length; i++) {
+            maximumLikelyhoodTraitStateDbl[i] /= initialSpeciesThatAreDescendants.size();
+
+            // If most descendants have trait present
+            if (maximumLikelyhoodTraitStateDbl[i] > 0.5) {
+                maximumLikelyhoodTraitStateInt[i] = 1;
+            } // If most descendants have trait absent or absence/presence is equal.
+            else if (maximumLikelyhoodTraitStateDbl[i] <= 0.5) {
+                maximumLikelyhoodTraitStateInt[i] = 0;
+            }
+        }
+
+        return maximumLikelyhoodTraitStateInt;
+    }
+
+
+
+    /**
+     * This recursive method determines all descendants, of the provided ancestor node, that were initial species entered
+     * at rooted tree instantiation. It does so by looking at all direct and indirect descendants of this species
+     * and placing them in the provided array list if their height is equal to 1.
+     * @param ancestor An ancestor node(species).
+     * @param initSpeciesThatAreDescendants An empty array list that will contain all initial species that
+     *      are descendants of this species.
+     */
+    private static void getInitialSpeciesThatAreDescendants(Species ancestor,
+        ArrayList<Species> initialSpeciesThatAreDescendants) {
+        // gets descendants of each ancestor node
+        Species leftDescendant = ancestor.getDescendants()[0];
+        Species rightDescendant = ancestor.getDescendants()[1];
+
+        // If left descendant is an ancestor node then recursively call this method to find its
+        // descendants
+        if (leftDescendant.getHeight() != 1) {
+            getInitialSpeciesThatAreDescendants(leftDescendant, initialSpeciesThatAreDescendants);
+        } else {
+            initialSpeciesThatAreDescendants.add(leftDescendant);
+        }
+
+        // If right descendant is an ancestor node then recursively call this method to find its
+        // descendants
+        if (rightDescendant.getHeight() != 1) {
+            getInitialSpeciesThatAreDescendants(rightDescendant, initialSpeciesThatAreDescendants);
+        } else {
+            initialSpeciesThatAreDescendants.add(rightDescendant);
+        }
+    }
+
+    /**
+     * This method finds the index of a specified trait state in a species possible trait state array
+     * @param traitState
+     * @param species
+     * @return
+     */
+    private static int getTraitStateIndex(int[] traitState, Species species) {
+
+        for (int i = 0; i < species.getPossibleTraitStates().length; i++) {
+            if (traitState.equals(species.getPossibleTraitStates()[i])) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+
+
+    /**
+     * This recursive method finds the trait states with the minimum parsimony score and stores the index of these states in the appropriate field
+     * Iterates from the bottom right to bottom left, then moves up a height value and repeats
+     * right to left until root is reached. Algorithm can be best understood by walking through
+     * the comments below and tracking the code.
+     * @param speciesIndex The index of the current species.
+     * @param speciesArr The array of ancestor nodes.
+     * @return The species index.
+     */
+    private static void findOptimalTraitStates(int speciesIndex, Species[] speciesArr) {
+        speciesIndex--;
+
+        // Exit condition to the recursive function.
+        // When we have gone through each ancestor node in the speciesArr, then we are done
+        // calculating for this tree and can start calculating the next one until all
+        // possible combinations have been solved/disproved to be the tree with minimum parsimony values.
+        if (speciesIndex < 0) {
+            return;
+        }
+
+        // Gets the species reference value from the speciesArr array
+        Species currentSpecies = speciesArr[speciesIndex];
+        // Loops through each possible trait for the current species in the speciesArr at
+        // speciesIndex
+        for (int i = 0; i < currentSpecies.getPossibleTraitStates().length; i++) {
+            currentSpecies.setTraits(currentSpecies.getPossibleTraitStates()[i]);
+            indexOfAncestorTracker[speciesIndex] = i;
+
+            int currParsimonyScore = getParsimonyScore(speciesArr, speciesIndex);
+            // Calculate parsimony and store the parsimony and its related data into the method
+            // fields if and only if this is the most parsiminious tree seen so far.
+            // Only occurs when the method reaches the root of the tree and rest of ancestor nodes
+            // in the ancestor node array have had their trait state determined for this tree.
+            if (speciesIndex == 0) {
+                // Compares to minimum parsimony score, if smaller we change minimum score and store
+                // the index of the trait states.
+                if (currParsimonyScore <= minParsimonyScore) {
+                    minParsimonyScore = currParsimonyScore;
+                    indexOfMinimumParsimonyTree = indexOfAncestorTracker.clone();
+                    System.out.println("~~!!~~!!~~!!~~!!New Minimum Parsimony Tree found at index " + i + " of the root species!!~~!!~~!!~~!!~~");
+                }
+            }
+            // Allows for backtracking, if at any point the partial tree has a parsimony score
+            // greater than the current minimum parsimony score of a full tree, stop iterating this path and
+            // go to the next one.
+            if (currParsimonyScore >= minParsimonyScore) {
+                continue;
+            }
+            // Displays program finding optimal trait states
+            for (int j = (speciesArr.length - 1 - speciesIndex); j > 0; j--) {
+                System.out.print("\t");
+            }
+            System.out.println("Calculating Parsimony for Trait State " + i + " of species index "
+                + speciesIndex + " and all subsequent trees");
+
+            // Recursive method call to determine the next ancestor nodes traits in the current tree
+            findOptimalTraitStates(speciesIndex, speciesArr);
+        }
+
+        return;
+    }
+
+    /**
+     * This method calculates the parsimony score of the entire tree. It works by calculating
+     * the parsimony score of each ancestor node to its descendants.
+     * @see Species.getParsimonyScoreToDescendants for information on how parsimony is calculated.
+     * @param speciesArr
+     * @param indexToIterateFrom
+     * @return int parsimony score from indexToIterate from until end of speciesArr
+     */
+    private static int getParsimonyScore(Species[] speciesArr, int indexToIterateFrom) {
+        int parsimonyScoreOfTree = 0;
+
+        for (int i = indexToIterateFrom; i < speciesArr.length; i++) {
+            parsimonyScoreOfTree += speciesArr[i].getParsimonyScoreToDescendants();
+        }
+
+        return parsimonyScoreOfTree;
+    }
+
+    /**
+     * This method changes the actual trait states of the ancestor nodes to the states determined to have minimum parsimony.
+     * @param ancestorNodes
+     */
+    private static void changeTreeTraitsToMinPars(Species[] ancestorNodes) {
+        for (int i = 0; i < ancestorNodes.length; i++) {
+            int[] optimalTraits =
+                ancestorNodes[i].getPossibleTraitStates()[indexOfMinimumParsimonyTree[i]];
+            ancestorNodes[i].setTraits(optimalTraits);
+        }
+    }
+
+    /**
+     * This method is used to get the parsimony score of the tree after findMostParsimoniousTree method
+     * has been called somewhere else in the program.
+     * @return The parsimony score of the most parsimonious tree.
+     */
+    public static int getParsimonyScoreOfTree() {
+        return minParsimonyScore;
+    }
+
+}
