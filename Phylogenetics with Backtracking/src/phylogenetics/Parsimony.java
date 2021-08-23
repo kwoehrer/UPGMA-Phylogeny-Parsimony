3d4e6185003734a36efe7daffb448679f5bc36ba
Krischan Woehrer <60715629+kwoehrer@users.noreply.github.com>
2021-08-23 14:06:38 -0700

    Update Parsimony.java

--- Phylogenetics with Backtracking/src/phylogenetics/Parsimony.java
+++ Phylogenetics with Backtracking/src/phylogenetics/Parsimony.java
@@ -12,32 +12,6 @@ import java.util.ArrayList;
 //
 ///////////////////////////////// NOTES ////////////////////////////////////////
 //
-// Reasons for creating this program:
-// 1. I remember taking microbiology-470 at UW-Madison and one of our big
-// projects for the semester was to create the most parsiminious
-// phylogenetic tree of 20-25 randomly named species when we were only given
-// the presence of various traits (I remember it being close to 10-12).
-// Everyone in the course spent hours upon hours manually crafting their
-// trees and I remember thinking to myself how nice it would be if a
-// computer could help compute the optimal state for parsimony.
-// Unfortunately, almost all the programs found online either costed money
-// or their algorithms were faulty and yielded trees that were close to
-// the most parsiminious tree but not quite there. Now that I am pursuing
-// a career in computer science and developement, I have decided to solve
-// this problem on my own and offer it as a free resource online.
-// 2. Gain a greater understanding of binary trees and their algorithms
-// 3. Practice my skills learned from my coursework in computer science.
-// Things to improve/TODO:
-// 1. Numerous points in the program could be better optimized to run a
-// little bit faster. These are noted locally when they occur.
-// 2. When determining the most parsiminious trait states, I currently use
-// a brute-force approach. Although I narrow down the number of computations
-// by determining traits of ancestors we know for certain, I believe there
-// are additional ways I can cut down on the number of computations and speed
-// the program up.
-// A brute-force approach will still be required in order construct
-// the most parsiminious phylogenetic tree accurately.
-//
 //
 ///////////////////////////////// CITATIONS ////////////////////////////////////
 //
