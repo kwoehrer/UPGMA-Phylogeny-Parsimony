0e397d9ec3ec2f3c60f1916c775561cdbbc03df2
Krischan Woehrer <60715629+kwoehrer@users.noreply.github.com>
2021-08-23 14:07:27 -0700

    Update Species.java

--- Phylogenetics with Backtracking/src/phylogenetics/Species.java
+++ Phylogenetics with Backtracking/src/phylogenetics/Species.java
@@ -9,29 +9,6 @@ package phylogenetics;
 //
 ///////////////////////////////// NOTES ////////////////////////////////////////
 //
-// Reasons for creating this program:
-//      1. I remember taking microbiology-470 at UW-Madison and one of our big
-//      projects for the semester was to create the most parsiminious
-//      phylogenetic tree of 20-25 randomly named species when we were only given
-//      the presence of various traits (I remember it being close to 10-12).
-//      Everyone in the course spent hours upon hours manually crafting their
-//      trees and I remember thinking to myself how nice it would be if a
-//      computer could help compute the optimal state for parsimony.
-//      Unfortunately, almost all the programs found online either costed money
-//      or their algorithms were faulty and yielded trees that were close to
-//      the most parsiminious tree but not quite there. Now that I am pursuing
-//      a career in computer science and development, I have decided to solve
-//      this problem on my own and offer it as a free resource online.
-//      2. Gain a greater understanding of binary trees and their algorithms
-//      3. Practice my skills learned from my coursework in computer science.
-//      4. Improve my ability to perform OOP
-//Things to improve/TODO:
-//      1. Numerous points in the program could be better optimized to run a
-//      little bit faster. These are noted locally when they occur.
-//      2. Create a GUI
-//      3. Pipeline this as a backend to a website/server and make this more
-//      accessible to people who aren't tech savvy.
-// 
 //
 ///////////////////////////////// CITATIONS ////////////////////////////////////
 //
